6fd0f9da311d4da79cac0ff697de40afbdb44f12
TongWenQian <839035680@qq.com>
2019-07-31 21:07:21 +0800

    基本完整还差暂停

--- js/main.js
+++ js/main.js
@@ -3,12 +3,15 @@ window.onload = function(){
   var imgArr = document.getElementsByTagName("img");
   var buttonWrapper = document.getElementById("buttonWrapper");
   var container = document.getElementsByClassName("container");
+  console.log(container);
   var allButtons = document.getElementsByClassName("buttonNumber");
+  var pre = document.getElementById("previous");
+  var next = document.getElementById("next");
   var index = 0;
-
-  //初始图片所在位置
+  //初始图片的长度
   imgList.style.width = 400*imgArr.length+"px";
-  console.log( imgList.style.width);
+  //图片开始位置
+  imgList.style.left = "-400px";
   //数字按钮的位置
   buttonWrapper.style.left = (container.offsetWidth-buttonWrapper.offsetWidth)/2;
   //获取数字按钮，设置颜色
@@ -17,44 +20,85 @@ window.onload = function(){
   for(var i=0;i<allButtons.length;i++){
     allButtons[i].num = i;
     allButtons[i].onclick = function(){
-       index = this.num;
+       index = this.num+1;
        setButton();
+ 
        imgList.style.left = -400*index+"px";
     }
   }
   //开启自动切换图片 
   autoChange(); 
+  //调用下一张，上一张功能
+  upAndDown(); 
+  function upAndDown(){
+    pre.onclick = function(){
+      index--;
+      imgList.style.left = -400*index+"px";
+      if(index === 0){
+        
+        index = imgArr.length-2
+        setTimeout(function(){   
+          imgList.style.transition = "none";
+          imgList.style.left="-1600px"; 
+      },1000); 
+      imgList.style.transition = "all 1s";
+      }
+      setButton();
+    }
+    next.onclick = function(){
+      index++;
+      imgList.style.left = -400*index+"px";
+      if(index == imgArr.length-1){
+        
+        index=1;
+        setTimeout(function(){   
+            imgList.style.transition = "none";
+            imgList.style.left="-400px"; 
+        },1000);   
+        imgList.style.transition = "all 1s";
+      }
+      setButton();
+    }
+  }
   //创建一个方法用来设置选中的button
   function setButton(){
     if(index >= imgArr.length-1){
       imgList.style.left = -400*index+"px";
-      var width = -(imgArr.length-1)*400+"px";
-      index=0;
+      index=1;
       setTimeout(function(){   
-          imgList.style.transition = "none" 
-          imgList.style.left=0; 
+          imgList.style.transition = "none";
+          imgList.style.left="-400px"; 
       },1000);      
     }
     for(var i=0;i<allButtons.length;i++){
        allButtons[i].style.backgroundColor = "";
     }
-    allButtons[index].style.backgroundColor = "#252526";
+    allButtons[index-1].style.backgroundColor = "#252526";
   }
-
-   function delayMoveToFirst(){
-
-   }
-
   function autoChange(){
     //开启定时器，定时切换图片
-    timer = setInterval (function(){
+    var timer = setInterval (function(){
       imgList.style.transition = "all 1s"
       index++;
-      index %= imgArr.length;
+      index %= (imgArr.length);
+      console.log("hahahaha"+index);
       imgList.style.left = -400*index+"px";
       setButton();
     },2000);
+    /** 
+    container[0].onmouseenter = function(){
+      console.log(1);
+      clearInterval(timer);
+    }
+    container[0].onmouseleave = function(){
+      timer = setInterval (function(){
+        imgList.style.transition = "all 1s";
+        index++;
+        index %= (imgArr.length);
+        console.log("hahahaha"+index);
+        imgList.style.left = -400*index+"px";
+        setButton();
+      },2000);
+    }*/
   }
-
-  
 }
